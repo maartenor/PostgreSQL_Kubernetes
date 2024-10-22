c9ef70fa1a1c00b2bc2c3a810f00891dcac2f294
maartenor <47463236+maartenor@users.noreply.github.com>
2024-10-22 23:36:28 +0200

    Changes to be committed: 	new file:   10_working_postgres.yaml 	new file:   11 postgresql-ns.txt 	renamed:    postgres-configmap.yaml -> 12 postgres-configmap.yaml 	renamed:    psql-pv.yaml -> 13_psql-pv.yaml 	renamed:    psql-claim.yaml -> 14_psql-claim.yaml 	renamed:    ps-service.yaml -> 15_ps-service.yaml 	renamed:    ps-deployment.yaml -> 19 ps-deployment.yaml

---
+++ create_dummy_databaset.sql
@@ -0,0 +1,16 @@
+CREATE TABLE IF NOT EXISTS public.created_table_2 (
+	column1 varchar NULL,
+	column2 varchar NULL
+);
+INSERT INTO public.created_table_2 (column1,column2) VALUES
+	 ('C1R1','C2R1'),
+	 ('C1R2','C2R2'),
+	 ('C1R3','C2R3'),
+	 ('C1R4','C2R4'),
+	 ('C1R5','C2R5'),
+	 (pg_catalog.concat('now: ',pg_catalog.clock_timestamp()),'C2R6'),
+	 (pg_catalog.concat('db: ',pg_catalog.current_database()),'C2R7' ),
+	 (pg_catalog.concat('schema: ',pg_catalog.current_schema()),'C2R8'),
+	 (pg_catalog.concat('user: ',pg_catalog."current_user"()),'C2R9'),
+	('C1R10','C2R10')
+;
